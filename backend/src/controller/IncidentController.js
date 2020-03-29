b5f61f27a5f645d90cdce023147c43dbe59757b7
mbagatini <morganabagatini@gmail.com>
2020-03-29 16:26:14 -0300

    Mobile do app

--- backend/src/controller/IncidentController.js
+++ backend/src/controller/IncidentController.js
@@ -19,7 +19,7 @@ module.exports = {
 
         res.header('X-Total-Count', count.count);
 
-        return res.json({incidents});
+        return res.json(incidents);
     },
 
     async store(req, res) {
